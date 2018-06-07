6e0a5e1287b47e36ac673564284192dfa96599a1
anudeepgupta42 <33155761+anudeepgupta42@users.noreply.github.com>
2018-06-07 21:35:46 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,27 +6,29 @@ Created on Thu May  3 18:03:52 2018
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import json, apiai
 application = Flask(__name__)
+cors = CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
+
 
 
 @application.route('/submit/<userText>',methods=['GET'])
+@cross_origin()
 def get_bot_response(userText):
-
-    """ Fetching user dialog from UI """
-    #userText = request.args.get('msg')
-    """airequest = Dialogflow_connection() 
-    
-    airequest.query = str(userText)
-    airesponse = airequest.getresponse()
-    raw_data = airesponse.read()
+	
+	""" Fetching user dialog from UI """
+	#userText = request.args.get('msg')
+	print(userText)
+	airequest = Dialogflow_connection()
+	airequest.query = str(userText)
+	airesponse = airequest.getresponse()
+	raw_data = airesponse.read()
 	# JSON default
-    encoding = airesponse.info().get_content_charset('utf8')  
-   
-    obj = json.loads(raw_data.decode(encoding))
-    
-    return jsonify(obj)"""
-    return jsonify(userText)
+	encoding = airesponse.info().get_content_charset('utf8')
+	obj = json.loads(raw_data.decode(encoding))
+	return jsonify(obj)
 
 def Dialogflow_connection():
     
@@ -39,3 +41,4 @@ def Dialogflow_connection():
 
 if __name__ == "__main__":
     application.run()
+
