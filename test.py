acda996f9dc128ae914a434e2404fde895f189de
anudeepgupta42 <33155761+anudeepgupta42@users.noreply.github.com>
2018-06-08 09:25:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,45 @@
+import urllib
+import json 
 
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 202034034, 1);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201485242, 2);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 200362533, 5);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201460992, 5);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201383464, 10);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201555227, 4);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201942067, 5);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201942047, 3);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201667054, 1);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201829182, 1);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 200067442, 2);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201470136, 5);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 200323749, 3);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 200362532, 6);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201470157, 2);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201029844, 2);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201276059, 2);
-INSERT INTO user_search (user_id, item_group_id, count) VALUES (2, 201855368, 2);
 
+select item_group_id, count where user_id = ''
+user_id='1'
+grp_id='202034034'
+cnt='2'
+
+
+data =  {
+
+        "Inputs": {
+
+                "input1":
+                {
+                    "ColumnNames": ["user_id", "item_group_id", "count"],
+                    "Values": [ [ user_id, grp_id, cnt ], ]
+                },        },
+            "GlobalParameters": {
+}
+    }
+
+body = str.encode(json.dumps(data))
+
+url = 'https://ussouthcentral.services.azureml.net/workspaces/449a9beb4cd9463cb93b54b0815a6cf4/services/4bece0ff30304c3eb30e22e97c910460/execute?api-version=2.0&details=true'
+api_key = 'x2v11VZSYsvp0OFKsaLmLl9P9hHgaH4ANvh4/0T3OO3T37zoOVL125Pf2pesVqDoGaDL3oaBfZvbxGub5Ga1Ew==' 
+# Replace this with the API key for the web service
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'method':'POST'}
+
+req = urllib.request.Request(url, body, headers) 
+
+response = urllib.request.urlopen(req)
+
+    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+    # req = urllib.request.Request(url, body, headers) 
+    # response = urllib.request.urlopen(req)
+
+result = response.read()
+encoding = response.info().get_content_charset('utf8')  # JSON default
+obj = json.loads(result.decode(encoding))
+recomm = obj['Results']['output1']['value']['Values']
+
+for rec in recomm:
+    print(rec)
